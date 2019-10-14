12218827faa70d7200737c177a9bb1dc17fd33c6
davyengone <davy@hackages.io>
2019-10-14 15:53:34 +0200

    splitted the auth routing

--- src/app/modules/AuthModule/auth.module.ts
+++ src/app/modules/AuthModule/auth.module.ts
@@ -1,15 +1,9 @@
+import { routes } from './auth.routing';
 import { LoginComponent } from './components/login/login.component';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [
-  {
-    path: '',
-    component: LoginComponent
-  }
-];
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [ReactiveFormsModule, CommonModule, RouterModule.forChild(routes)],
---
+++ src/app/modules/AuthModule/auth.routing.ts
@@ -0,0 +1,8 @@
+import { LoginComponent } from './components/login/login.component';
+import { Routes } from '@angular/router';
+export const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  }
+];
